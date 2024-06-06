bf6cddc206a2a7189be3412da21638f88287c909
ayumu izumi <ayumuizumi@ayumunoMac-mini.local>
2024-06-06 22:37:02 +0900

    fix can't connect keyboard when pc boot

--- keyboards/hillside/46/keymaps/vial/config.h
+++ keyboards/hillside/46/keymaps/vial/config.h
@@ -6,7 +6,10 @@
 // Some boards have issues detecting handedness using the vbus voltage.
 // Such as Elite-C v3, but not v3.1 on. Also apparently some ProMicro boards.
 // For those boands, use usb detection instead.
-// #define SPLIT_USB_DETECT
+#define SPLIT_USB_DETECT
+#define SPLIT_USB_TIMEOUT 5000
+#define SPLIT_USB_TIMEOUT_POLL 30
+
 #define DYNAMIC_KEYMAP_LAYER_COUNT 6
 #define VIAL_KEYBOARD_UID {0x8A, 0x71, 0x42, 0xA2, 0xF0, 0x99, 0xA5, 0x24}
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 4 }
